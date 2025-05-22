d7a183cdd948af0b32e6f07574fda1c525299093
Nico Doebele <Nico.Doebele@Student.Reutlingen-University.de>
2025-05-22 09:36:29 +0200

    feat: make k3s version fixed

--- setup_k3s/bootstrap_master.sh
+++ setup_k3s/bootstrap_master.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo mkdir -p /etc/rancher/k3s/
-sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--tls-san $1" sh -s - server --cluster-init 
+sudo curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.32.3+k3s1" INSTALL_K3S_EXEC="--tls-san $1" sh -s - server --cluster-init 
