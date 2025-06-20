1a332c30de937d54a5cefeff977f2033d65a594c
Nico Doebele <Nico.Doebele@Student.Reutlingen-University.de>
2025-06-20 20:52:25 +0200

    fix: dont require mongo uri during build

--- twutter/src/lib/env.ts
+++ twutter/src/lib/env.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  MONGODB_URI: z.string().min(1, "MONGODB_URI is required"),
+  //MONGODB_URI: z.string().min(1, "MONGODB_URI is required"),
+  MONGODB_URI: z.string(),
 });
 
 const env = envSchema.parse(process.env);
